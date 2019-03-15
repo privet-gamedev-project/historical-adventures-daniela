1c1d5d7d4d30f1e47f0061188ffefc1da277e5cc
gammafp <gammafp@gmail.com>
2019-03-15 10:13:14 +0100

    Función para obtener el lenguaje del navegador (funciona en móvil)

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -2,3 +2,16 @@
 En helpers se hacen funciones de ayuda, una función de ayuda puede ser por ejemplo 
 una función que convierta array bi-dimensionales a array de una dimensión.
 */
+
+/**
+ *  Para los múltilpes idiomas que hay en España retorna solo español, en caso contrario retornará inglés (se puede modificar para agregar más idiomas)
+ *  @return String
+ */
+const getLang = () => (["ca", "gl", "es", "eu"]
+        .indexOf(navigator.language.split('-')[0]) != -1) ?
+    "es" :
+    "en";
+
+export default {
+    getLang
+};
