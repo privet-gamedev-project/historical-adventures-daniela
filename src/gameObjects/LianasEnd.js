c273540b5283ced8f2cf5eaab5c186866903789a
Barbara Bris <barbrilue@gmail.com>
2019-04-05 19:06:11 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/gameObjects/LianasEnd.js
@@ -0,0 +1,25 @@
+class LianasEnd extends Phaser.Physics.Arcade.Group {
+    constructor(world, scene, children, spriteArray) {
+        super(world, scene, children);
+        // this.scene = scene;
+
+        // create our enemies from the sprite array
+        this.createLianas(scene, spriteArray);
+    }
+    createLianas(scene, spriteArray) {
+        
+        spriteArray.map((sprite) => {
+            this.add(sprite);
+
+            this.configureLianasEnd(sprite,spriteArray);
+        });
+        
+    }
+    configureLianasEnd(liana,spriteArray){
+        liana.body.setAllowGravity(false);
+        liana.body.setSize(1,1);
+        liana.alpha=0;
+    }
+}
+
+export default LianasEnd;
